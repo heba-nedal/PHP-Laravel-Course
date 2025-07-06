06dae88f4636bde1bbf23c771c94d4b1a323814c
heba-nedal <hebanedal608@gmail.com>
2025-07-06 11:35:13 +0300

    Route&Controller

---
+++ app/Http/Controllers/SiteOne/SiteOneController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\SiteOne;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SiteOneController extends Controller
+{
+
+    function home()
+    {
+        echo '<h1>Home</h1>';
+        echo '<h1>Home</h1>';
+        echo '<h1>Home</h1>';
+        echo '<h1>Home</h1>';
+        echo '<h1>Home</h1>';
+        echo '<h1>Home</h1>';
+    }
+
+    function about()
+    {
+        echo '<h1>About</h1>';
+        echo '<h1>About</h1>';
+        echo '<h1>About</h1>';
+        echo '<h1>About</h1>';
+        echo '<h1>About</h1>';
+        echo '<h1>About</h1>';
+    }
+
+    function contact()
+    {
+        echo '<h1>Contact</h1>';
+        echo '<h1>Contact</h1>';
+        echo '<h1>Contact</h1>';
+        echo '<h1>Contact</h1>';
+        echo '<h1>Contact</h1>';
+        echo '<h1>Contact</h1>';
+    }
+
+    function msg($id)
+    {
+        //  return view('SiteOne.msg1')->with('id', $id);
+        return view('SiteOne.msg2')->with('id', $id);
+    }
+}
